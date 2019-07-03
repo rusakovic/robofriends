f29fcbedad3c580112b1c60d20bfcd6b44515f74
maxrusakovich <maxrusakovich@gmail.com>
2019-07-03 12:48:33 +0300

    Updates

---
+++ precache-manifest.994b2b1174e27051499093db41314b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e019aba3e33067f6fbf2abd5bea8bb4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "46ba31effbcf20610e91",
+    "url": "/robofriends/static/css/main.d2ac4019.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robofriends/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "46ba31effbcf20610e91",
+    "url": "/robofriends/static/js/main.f206ea3c.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
